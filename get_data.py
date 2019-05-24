96bb78f60ba90e7e2ba7c313a32d6115d7756613
Sumitro Bhaumik <sumitros@gmail.com>
2019-05-24 17:21:47 +0530

    bugz

--- get_data.py
+++ get_data.py
@@ -40,7 +40,7 @@ class Skidstorm_App:
 
 def main():
 
-	if os.environ.get() == 'heroku':
+	if os.environ.get('APP_LOCATION') == 'heroku':
 		run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 	else:
 		run(host='localhost', port=8080, debug=True)
